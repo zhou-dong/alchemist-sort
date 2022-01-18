651eb9ac409af57f3fc6d1f68c586436bed2374c
Dong Zhou <dozhou@rubiconproject.com>
2022-01-17 23:29:05 -0800

    Update Bubble.tsx

--- src/sorts/Bubble.tsx
+++ src/sorts/Bubble.tsx
@@ -6,7 +6,7 @@ import Cube from '../models/cube';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const cube = new Cube().setColor('red');
+const cube = new Cube().setColor('green');
 
 scene.add(cube);
 
