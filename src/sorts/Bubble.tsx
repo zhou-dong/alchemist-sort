35bfcc5dfd91dbd7c969e538a34fb341c1ac7a2e
Dong Zhou <dozhou@rubiconproject.com>
2022-01-18 02:34:49 -0800

    Update Bubble.tsx

--- src/sorts/Bubble.tsx
+++ src/sorts/Bubble.tsx
@@ -7,20 +7,22 @@ interface Params {
     setScene: React.Dispatch<React.SetStateAction<THREE.Scene | undefined>>;
 }
 
+const size = 5;
+const colors = generateColor("#9FE2BF", "#40E0D0", size);
+const scene = new THREE.Scene(); //React.useMemo(() => new THREE.Scene(), []);
+
+for (let i = 0; i < size; i++) {
+    console.log("bubble", i);
+
+    const cube = new Cube()
+        .setColor(colors[i])
+        .setWidth(1)
+        .setHeight((i + 1) * 1);
+    cube.position.setX(i - 5 + 1 * i);
+    scene.add(cube);
+}
+
 function Bubble({ setScene }: Params) {
-
-    const size = 5;
-    const colors = generateColor("#9FE2BF", "#40E0D0", size);
-    const scene = React.useMemo(() => new THREE.Scene(), []);
-
-    for (let i = 0; i < size; i++) {
-        const cube = new Cube()
-            .setColor(colors[i])
-            .setWidth(1)
-            .setHeight((i + 1) * 1);
-        cube.position.setX(i - 5 + 1 * i);
-        scene.add(cube);
-    }
 
     React.useEffect(() => {
         setScene(scene);
