ca49c5ce264f7db17a83c1de1cf61ce7c41c164f
Dong Zhou <dozhou@rubiconproject.com>
2022-01-17 23:28:49 -0800

    Update Bubble.tsx

--- src/sorts/Bubble.tsx
+++ src/sorts/Bubble.tsx
@@ -24,8 +24,8 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 };
 animate();
