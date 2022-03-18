c5eb998ebd93cb92374f973af3f2012b6bf462a9
Dong Zhou <dozhou@rubiconproject.com>
2022-03-18 03:00:45 -0700

    implement bubble sort

---
+++ src/sorts/algo.ts
@@ -0,0 +1,30 @@
+import { Action } from "../models/action";
+import Container from "../models/container"
+import { Step } from "../models/step"
+
+const exchange = (arrays: Container[], a: number, b: number): void => {
+    const temp = arrays[a];
+    arrays[a] = arrays[b];
+    arrays[b] = temp;
+}
+
+export const sort = (arrays: Container[]): Step[] => {
+    const steps: Step[] = [];
+
+    for (let i = arrays.length - 1; i >= 0; i--) {
+        for (let y = 0; y < i; y++) {
+            const a = arrays[y];
+            const b = arrays[y + 1];
+            if (a.payload > b.payload) {
+                const step: Step = { a, b, action: Action.Switch };
+                steps.push(step);
+                exchange(arrays, y, y + 1);
+            } else {
+                const step: Step = { a, b, action: Action.None };
+                steps.push(step);
+            }
+        }
+    }
+
+    return steps;
+}
