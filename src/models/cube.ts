97cceabc69c8cf858a1e7b9ba1cbc6ddf38d8a50
Dong Zhou <dozhou@rubiconproject.com>
2022-05-01 02:32:47 -0700

    Update cube.ts

--- src/models/cube.ts
+++ src/models/cube.ts
@@ -5,9 +5,8 @@ export default class Cube extends THREE.Mesh implements Container {
 
     payload: number;
 
-    constructor(payload: number, color: THREE.ColorRepresentation, width: number, height: number, depth: number) {
+    constructor(payload: number, material: THREE.Material, width: number, height: number, depth: number) {
         const geometry = new THREE.BoxGeometry(width, height, depth);
-        const material = new THREE.MeshStandardMaterial({ color, emissive: "#304a4b", roughness: 0.6, metalness: 0.4 });
         super(geometry, material)
         this.payload = payload;
     }
