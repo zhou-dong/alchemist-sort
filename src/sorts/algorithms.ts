eb2e9b603fa3ad92c6aa0bbf9dc2bcfa789c78ec
Dong Zhou <dozhou@rubiconproject.com>
2022-05-07 19:10:42 -0700

    Create algorithms.ts

---
+++ src/sorts/algorithms.ts
@@ -0,0 +1,38 @@
+import Algorithm from "../models/algorithm";
+
+const algorithms: Algorithm[] = [
+    {
+        name: "Bubble Sort",
+        path: "/bubble-sort"
+    },
+    {
+        name: "Selection Sort",
+        path: "/selection-sort"
+    },
+    {
+        name: "Merge Sort",
+        path: "/merge-sort"
+    },
+    {
+        name: "Insertion Sort",
+        path: "/insertion-sort"
+    },
+    {
+        name: "Quick Sort",
+        path: "/quick-sort"
+    },
+    {
+        name: "Heap Sort",
+        path: "/heap-sort"
+    },
+    {
+        name: "Counting Sort",
+        path: "/counting-sort"
+    },
+    {
+        name: "Bucket Sort",
+        path: "/bucket-sort"
+    },
+]
+
+export default algorithms;
