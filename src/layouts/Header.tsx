36ab5bdadf6381aad686c0660bf4716ec676b12a
Dong Zhou <dozhou@rubiconproject.com>
2022-05-07 21:41:16 -0700

    Update Header.tsx

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Tab, Tabs } from "@mui/material";
 import { Link, matchPath, useLocation } from 'react-router-dom';
-import Algorithm from "../models/algorithm";
 import algorithms from "../sorts/algorithms";
 
 function useRouteMatch(patterns: readonly string[]) {
@@ -17,14 +16,10 @@ function useRouteMatch(patterns: readonly string[]) {
     return null;
 }
 
-const buildTable = (index: number, { name, path }: Algorithm) => (
-    <Tab key={index} component={Link} label={name} value={path} to={path} />
-);
-
 export default function () {
 
     const paths: string[] = algorithms.map(algorithm => algorithm.path);
-    const tabs = algorithms.map((algorithm, index) => buildTable(index, algorithm));
+    const tabs = algorithms.map(({ name, path }, index) => <Tab key={index} component={Link} label={name} value={path} to={path} />);
 
     const routeMatch = useRouteMatch(paths);
     const currentTab = routeMatch?.pattern?.path;
@@ -41,7 +36,7 @@ export default function () {
     );
 
     return (
-        <AppBar elevation={0}>
+        <AppBar color="secondary" elevation={0}>
             {navs}
         </AppBar>
     );
