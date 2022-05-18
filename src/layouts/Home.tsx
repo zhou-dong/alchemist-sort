f2df23b5d69b309dc870aff6e884afd2d112437a
Dong Zhou <dozhou@rubiconproject.com>
2022-05-18 05:01:15 -0700

    Update Home.tsx

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { Typography } from "@mui/material"
 import { styled } from '@mui/material/styles';
 import HomeIcon from '@mui/icons-material/Home';
+import React from 'react';
 
 const Center = styled("div")(() => ({
     border: 0,
@@ -12,6 +13,7 @@ const Center = styled("div")(() => ({
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    zIndex: 100
 }));
 
 interface Params {
@@ -21,10 +23,10 @@ interface Params {
 const scene = new THREE.Scene();
 
 const Home = ({ setScene }: Params) => {
-    setScene(scene);
+    React.useEffect(() => { setScene(scene) }, [setScene]);
     return (
         <Center>
-            <HomeIcon sx={{ fontSize: 110 }} color="secondary" />
+            <HomeIcon sx={{ fontSize: 100 }} color="secondary" />
             <Typography align="center" variant="h1" color="secondary">
                 Sorting
             </Typography>
