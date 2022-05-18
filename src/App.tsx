897a5273a2ce297b104cdb045ffa65dfa5161827
Dong Zhou <dozhou@rubiconproject.com>
2022-05-18 05:01:24 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,8 @@ import { Route, Routes } from 'react-router-dom';
 import Bubble from './sorts/bubble/Bubble';
 import Demo from "./sorts/bubble/Demo";
 import Header from "./layouts/Header";
-import { clearIntersection, raycaster, render, renderer, refreshRaycaster } from './utils/render';
-
-interface HomeParams {
-  setScene: React.Dispatch<React.SetStateAction<THREE.Scene | undefined>>
-}
-
-const homeScene = new THREE.Scene();
-
-function Home({ setScene }: HomeParams) {
-
-  React.useEffect(() => {
-    setScene(homeScene);
-  }, [])
-
-  return (<></>);
-}
+import { render, renderer } from './utils/render';
+import Home from './layouts/Home';
 
 function App() {
 
@@ -36,20 +22,7 @@ function App() {
 
   function animate() {
     requestAnimationFrame(animate);
-
     if (scene) {
-      refreshRaycaster();
-      const intersects = raycaster.intersectObjects(scene.children);
-
-      for (let i = 0; i < intersects.length; i++) {
-        const intersect = intersects[i];
-        // (intersect.object as any).material.color.set("red");
-      }
-
-      if (intersects.length > 0) {
-        clearIntersection();
-      }
-
       render(scene);
     }
   };
