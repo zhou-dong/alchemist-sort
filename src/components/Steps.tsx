74094dd81d1b7968ff18eaf067466145d808a7d9
Dong Zhou <dozhou@rubiconproject.com>
2022-05-17 21:55:33 -0700

    Update Steps.tsx

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,28 +1,16 @@
+import { Toolbar } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import { createStyles, makeStyles } from "@mui/styles";
 
 interface Props {
     steps: number;
 }
 
-const useStyles = makeStyles(() => createStyles({
-    stepsContainer: {
-        position: "fixed",
-        top: 70,
-        right: 40,
-    },
-}));
-
 export default function ({ steps }: Props) {
-    const { stepsContainer } = useStyles();
     return (
-        <div className={stepsContainer}>
-            <Typography color="primary" variant='body1' style={{ marginLeft: "-4px" }}>
-                STEPS
-            </Typography>
-            <Typography color="secondary" variant="h4" style={{ marginTop: "-9px" }}>
+        <Toolbar sx={{ position: "fixed", top: 60, right: 0 }}>
+            <Typography color="secondary" variant="h4">
                 {(steps < 10) ? "0" : ""}{steps}
             </Typography>
-        </div>
+        </Toolbar>
     );
 }
